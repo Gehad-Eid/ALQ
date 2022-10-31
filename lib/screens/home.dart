34a2fe35293b3785da5a67238731ea595892c704
amirah14h <98522507+amirah14h@users.noreply.github.com>
2022-10-31 21:09:06 +0500

    Add files via upload

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,3 +1,4 @@
+import 'package:alqgp/screens/signup.dart';
 import 'package:flutter/material.dart';
 
 import '../consts.dart';
@@ -12,10 +13,29 @@ class WelcomeScreen extends StatefulWidget {
 class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color.fromARGB(255, 245, 243, 237),
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 254, 177, 217),
+      appBar: AppBar(title: Text("HomePage"), backgroundColor: Colors.pinkAccent,),
       //start here
-      body: Text("just a test"),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+             Image.asset("images/logo.jpeg", width:300, height:500),
+         Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+           Container( width:120,height: 50,child: ElevatedButton(onPressed: (){}, child: Text("Sign In"), style: ElevatedButton.styleFrom(primary: Colors.pinkAccent),)),
+          Container(margin: EdgeInsets.only(left: 30),width:120,height: 50,child: ElevatedButton(onPressed: (){
+            Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) { 
+              return sign_up();
+             }));
+          }, child: Text("Sign Up"), style: ElevatedButton.styleFrom(primary: Colors.pinkAccent),))
+         ],)
+        ],),
+      ),
     );
   }
 }
