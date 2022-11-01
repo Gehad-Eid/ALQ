2381912fc6da1ce79d47dc979c571cf6e23ce1c7
jawaherHejab <117159957+jawaherHejab@users.noreply.github.com>
2022-11-01 15:39:17 +0300

    log in function

---
+++ lib/screens/login.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:alqgp/widgets/my_button.dart';
+
+class SignInScreen extends StatefulWidget {
+  const SignInScreen({Key? key}) : super(key: key);
+
+  @override
+  _SignInScreenState createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              height: 180,
+              child: Image.asset('images/logo.jpeg'),
+            ),
+            SizedBox(height: 50),
+            TextField(
+              textAlign: TextAlign.center,
+              onChanged: (value) {},
+              decoration: const InputDecoration(
+                hintText: 'Enter your Email',
+                contentPadding: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 20,
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Color.fromARGB(255, 156, 22, 22),
+                    width: 1,
+                  ),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Color.fromARGB(255, 22, 70, 124),
+                    width: 2,
+                  ),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 8),
+            TextField(
+              textAlign: TextAlign.center,
+              onChanged: (value) {},
+              decoration: const InputDecoration(
+                hintText: 'Enter your password',
+                contentPadding: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 20,
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Color.fromARGB(255, 156, 22, 22),
+                    width: 1,
+                  ),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Color.fromARGB(255, 22, 70, 124),
+                    width: 2,
+                  ),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            MyButton(
+              color: Color.fromARGB(255, 22, 70, 124)!,
+              title: 'Sign in',
+              onPressed: () {},
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
