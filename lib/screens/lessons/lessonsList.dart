a508da45882a762e849809971dfedcc9a0ca7206
Gehad Salah <98451075+Gehad1995@users.noreply.github.com>
2022-11-02 17:26:55 +0300

    lessons start

--- lib/screens/lessons/lessonsList.dart
+++ lib/screens/lessons/lessonsList.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
 
+class Lessons extends StatefulWidget {
+  const Lessons({super.key});
+
+  @override
+  State<Lessons> createState() => _LessonsState();
+}
+
+class _LessonsState extends State<Lessons> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
